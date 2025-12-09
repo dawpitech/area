7654475a9ed99e0ccf316a27c52685fd86935f0f
Nathan Neveu <neveunathan33@gmail.com>
2025-12-09 18:06:27 +0100

    feat(apps/web) : Tiny message on signup page

--- apps/web/src/components/auth/register/index.jsx
+++ apps/web/src/components/auth/register/index.jsx
@@ -45,8 +45,15 @@ const Register = () => {
       <main className="w-full h-screen flex self-center place-content-center place-items-center">
         <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
           <div className="text-center mb-6">
-            <div className="mt-2">
-              <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
+            <div className="text-center mb-6">
+             <div className="mt-2">
+                <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">
+                  Create a New Account
+                </h3>
+                <p className="text-gray-500 text-sm mt-1">
+                  After signing up, you will be sent to the login page.
+                </p>
+              </div>
             </div>
           </div>
           <form
