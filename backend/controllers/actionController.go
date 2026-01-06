f750e8a94dde3bb725b1608ad1bc729b6645ce06
Antoine <me@antoinebellanger.fr>
2026-01-06 18:32:03 +0100

    fix(back): better logging on get info routes

--- backend/controllers/actionController.go
+++ backend/controllers/actionController.go
@@ -5,11 +5,13 @@ import (
 	"dawpitech/area/services"
 	"github.com/gin-gonic/gin"
 	"github.com/juju/errors"
+	"log"
 )
 
 func GetActionInfo(_ *gin.Context, in *routes.RequestGetActionInfo) (*routes.ResponseGetActionInfo, error) {
 	action, ok := services.ActionStore[in.ActionName]
 	if !ok {
+		log.Printf("Request for action info failed, action ('%s') not found.", in.ActionName)
 		return nil, errors.NotFound
 	}
 
--- backend/controllers/reactionController.go
+++ backend/controllers/reactionController.go
@@ -5,11 +5,13 @@ import (
 	"dawpitech/area/services"
 	"github.com/gin-gonic/gin"
 	"github.com/juju/errors"
+	"log"
 )
 
 func GetReactionInfo(_ *gin.Context, in *routes.RequestGetReactionInfo) (*routes.ResponseGetReactionInfo, error) {
 	reaction, ok := services.ReactionStore[in.ReactionName]
 	if !ok {
+		log.Printf("Request for reaction info failed, reaction ('%s') not found.", in.ReactionName)
 		return nil, errors.NotFound
 	}
 
