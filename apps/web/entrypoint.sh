040723f73efe9571f94eddf0ff78f9f9d96b4c33
Antoine <me@antoinebellanger.fr>
2026-01-14 14:58:30 +0100

    fix(ops): REACT_APP_API_URL baking is run at image build stage

---
+++ apps/web/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+sed -i 's|REACT_APP_API_URL_PLACEHOLDER|'${REACT_APP_API_URL}'|g' /usr/share/nginx/html/static/js/*.js
+
+cp /apk/client.apk public/. || true
+
+ngninx -g 'daemon off;'
